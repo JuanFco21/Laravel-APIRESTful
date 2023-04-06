af73a4927ee81f945726775d2d5297d490ec75d2
JuanFco21 <pacotab11@gmail.com>
2023-04-06 16:35:25 -0600

    Creacion de end-points usando rutas de Laravel

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Transaction\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +20,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
+Buyers
+*/
+Route::apiResource('buyers', BuyerController::class)->only(['index', 'show']);
+/*
+Categories
+Laravel 5.3 Route::resource('categories', 'Buyer\CategoryController');
+*/
+Route::apiResource('categories', CategoryController::class); 
+/*
+Products
+Laravel 5.3 Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+*/
+Route::apiResource('products', ProductController::class)->only(['index', 'show']); 
+/*
+Transactions
+*/
+Route::apiResource('transactions', TransactionController::class)->only(['index', 'show']);
+/*
+Sellers
+*/
+Route::apiResource('sellers', SellerController::class)->only(['index', 'show']); 
+/*
+Users
+*/
+Route::apiResource('users', UserController::class);
