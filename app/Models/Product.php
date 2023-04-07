d88abf2130ba8dcfa11689ba6473a4ccdc3dca54
JuanFco21 <pacotab11@gmail.com>
2023-04-06 19:41:02 -0600

    Implementacion de propiedades y relaciones

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,41 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Seller;
+use App\Models\Category;
+use App\Models\Transaction;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
+
+    const PRODUCTO_DISPONIBLE = 'disponible';
+    const PRODUCTO_NO_DISPONIBLE = 'no disponible';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function estaDisponible(){
+        return $this->status == Product::PRODUCTO_DISPONIBLE;
+    }
+
+    public function seller(){
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function transactions(){
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
 }
