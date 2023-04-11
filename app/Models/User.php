b12fabb27e84c53a63ef120d9b097f0a66dcc93b
JuanFco21 <pacotab11@gmail.com>
2023-04-10 19:49:38 -0600

    Se crearon mutadores y accesores para los campos name e email del modelo user

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -32,7 +33,6 @@ class User extends Authenticatable
         'verification_token',
         'admin',
     ];
-
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -44,6 +44,21 @@ class User extends Authenticatable
         'verification_token',
     ];
 
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $valor) => ucwords($valor),
+            set: fn (string $valor) => strtolower($valor),
+        );
+    }
+
+    protected function email(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $valor) => strtolower($valor),
+            set: fn (string $valor) => strtolower($valor),
+        );
+    }
     /**
      * The attributes that should be cast.
      *
@@ -53,15 +68,18 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function esVerificado(){
+    public function esVerificado()
+    {
         return $this->verified == User::USUARIO_VERIFICADO;
     }
 
-    public function esAdministrador(){
+    public function esAdministrador()
+    {
         return $this->admin == User::USUARIO_ADMINISTRADOR;
     }
 
-    public static function generarVerificationToken(){
+    public static function generarVerificationToken()
+    {
         return Str::random(40);
     }
 }
